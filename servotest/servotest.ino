0a25673017dd69edc8172247e9d08ceaae77d619
Nikola <nikola@Calcifer>
2016-10-25 16:50:15 +0100

    added some led functionality to the servo!

--- servotest/servotest.ino
+++ servotest/servotest.ino
@@ -4,9 +4,9 @@ Servo myservo;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);
+  pinMode(13, OUTPUT);
 
-  //set the default value to zero
+  myservo.attach(9);
   myservo.write(0);
 
   Serial.println("Hello, please enter a value between 0 and 220!");
@@ -15,13 +15,18 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     String cmdString = Serial.readString();
-
+  
     if (cmdString == "boop") {
+
+      flicker_led();
+      
       for (int pos = 0; pos <= 180; pos += 1) {
         myservo.write(pos);
         delay(15);
       }
 
+      flicker_led();
+      
       for (int pos = 180; pos <= 0; pos -= 1) {
         myservo.write(pos);
         delay(15);
@@ -30,8 +35,15 @@ void loop() {
     else {
       Serial.println("Moving to value: " + cmdString);
       myservo.write(cmdString.toInt());
+      flicker_led();
     }
   }
 
+}
 
+void flicker_led() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
 }
